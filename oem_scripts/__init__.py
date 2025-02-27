d514b70cde49df7af11a79715e46d677014d5053
Artur Pak <artur.pak@canonical.com>
2025-02-27 14:46:46 +0800

    releasing package oem-scripts version 2.42

--- oem_scripts/__init__.py
+++ oem_scripts/__init__.py
@@ -25,7 +25,7 @@ import subprocess
 from logging import debug, info, error, critical
 from tempfile import TemporaryDirectory
 
-__version__ = "2.41"
+__version__ = "2.42"
 
 ALLOWED_KERNEL_META_LIST = (
     "linux-oem-20.04d",
