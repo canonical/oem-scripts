635c4e5fd00f4231dda8e6fde7a41876edb0c93a
Dirk Su <dirk.su@canonical.com>
2026-06-04 11:17:28 +0800

    releasing package oem-scripts version 2.70

--- oem_scripts/__init__.py
+++ oem_scripts/__init__.py
@@ -25,7 +25,7 @@ import subprocess
 from logging import debug, info, error, critical
 from tempfile import TemporaryDirectory
 
-__version__ = "2.68"
+__version__ = "2.70"
 
 ALLOWED_KERNEL_META_LIST = (
     "linux-oem-20.04d",
